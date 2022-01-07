5249b1e9c5c927cf2557e9ba88d952107b485c9c
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-07 09:49:43 +0200

    classes
    quick class course on LinkedIn

---
+++ Fundamentals/objectAndClasses/React.js
@@ -0,0 +1,11 @@
+const { Component } = require("react");
+
+import React { Component} from "react";
+
+class Botton extends Component {
+    render() {
+        return ( <button>Hello</button>)
+    }
+}
+
+export default Botton
