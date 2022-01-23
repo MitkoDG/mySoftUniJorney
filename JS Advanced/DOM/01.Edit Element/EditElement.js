1b7aabb94d76cbe060b66050e0e59dbcf5b3dc54
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-23 22:03:41 +0200

    DOM lecture
    lecture

---
+++ JS Advanced/DOM/01.Edit Element/EditElement.js
@@ -0,0 +1,13 @@
+// function editElement(ref, match, replacer) {
+//         const content = ref.textContent;
+//         const matcher = new RegExp(match, 'g');
+//         const edited = content.replace(matcher, replacer);
+//         ref.textContent = edited;      
+// }
+
+function editElement(ref, match, replacer) {
+    const content = ref.textContent;
+    const matcher = new RegExp(match , 'g');
+    const edited = content.replace(matcher, replacer);
+    ref.textContent = edited
+}
