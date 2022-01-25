214af37be56d27f6bc74b41d7066be64388db100
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-25 16:46:29 +0200

    DOM

--- JS Advanced/DOM/01.Edit Element/EditElement.js
+++ JS Advanced/DOM/01.Edit Element/EditElement.js
@@ -1,13 +1,17 @@
-// function editElement(ref, match, replacer) {
-//         const content = ref.textContent;
-//         const matcher = new RegExp(match, 'g');
-//         const edited = content.replace(matcher, replacer);
-//         ref.textContent = edited;      
-// }
-
 function editElement(ref, match, replacer) {
-    const content = ref.textContent;
-    const matcher = new RegExp(match , 'g');
-    const edited = content.replace(matcher, replacer);
-    ref.textContent = edited
+        const content = ref.textContent;
+        const matcher = new RegExp(match, 'g');
+        const edited = content.replace(matcher, replacer);
+        ref.textContent = edited;      
 }
+
+function editElement(element, match, replacer) {
+    while(element.textContent.includes(match)) {
+        element.textContent = element.textContent.replace(match, replacer);
+    }
+}
+
+//     // regex solution
+//     let pattern = new RegExp(match, 'g')
+//     element.textContent = element.textContent.replace(pattern, replacer);
+// };
--- JS Advanced/DOM/1-edit-element.js
+++
@@ -1,9 +0,0 @@
-function solve(element, match, replacer) {
-    while(element.textContent.includes(match)) {
-        element.textContent = element.textContent.replace(match, replacer);
-    }
-
-    // regex solution
-    // let pattern = new RegExp(match, 'g')
-    // element.textContent = element.textContent.replace(pattern, replacer);
-};
