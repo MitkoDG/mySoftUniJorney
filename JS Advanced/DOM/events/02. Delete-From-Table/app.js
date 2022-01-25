a46fe6161dd7ceae442e03c7f08c1b7fa0fccd84
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-25 21:55:17 +0200

    DOM

---
+++ JS Advanced/DOM/events/02. Delete-From-Table/app.js
@@ -0,0 +1,19 @@
+function deleteByEmail() {
+  let emailInputElement = document.querySelector('input[name="email"]');
+
+  let emailCellElement = document.querySelectorAll("tr td:nth-of-type(2)");
+  let resultElement = document.getElementById("result");
+  let emailElements = Array.from(emailCellElement);
+  let targetElement = emailElements.find(
+    (x) => x.textContent == emailInputElement.value
+  );
+  // targetElement.parentNode.removeChild(targetElement) the old way of deleting
+
+  if (targetElement) {
+      targetElement.parentNode.remove();
+      resultElement.textContent = 'Deleted.'
+
+  } else {
+    resultElement.textContent = 'Not found'
+  }
+}
