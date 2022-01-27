1c36eecddd97c51595e07041f2ce091ccbc65ec1
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-27 23:00:41 +0200

    DOM

---
+++ JS Advanced/DOM/events/07. Distance-Converter/distanceConverter.js
@@ -0,0 +1,32 @@
+function attachEventsListeners() {
+  let [inputField, outputField] =
+    document.querySelectorAll('input[type="text"]');
+
+    // let fromOption = 
+  //missingcode
+
+  let convert = Number(inputField.value);
+  let result = 0;
+
+  switch (fromOption.value) {
+    case "km":
+      break;
+    case "m":
+      break;
+    case "cm":
+      break;
+    case "mm":
+      break;
+    case "mi":
+      break;
+    case "yrd":
+      break;
+    case "ft":
+      break;
+    case "in":
+      break;
+  }
+
+}
+
+
