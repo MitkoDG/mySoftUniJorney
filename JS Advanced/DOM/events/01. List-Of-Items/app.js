c9b9031ac82e243b078ff1ec5d83b1d32309ebf7
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-28 16:49:55 +0200

    DOM

--- JS Advanced/DOM/events/01. List-Of-Items/app.js
+++ JS Advanced/DOM/events/01. List-Of-Items/app.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    document.getElementById('error').textContent = '';
+    if (!document.getElementById('newItemText').value == '') {
+        let text = document.getElementById('newItemText').value;
+        let li = document.createElement('li')
+        li.appendChild(document.createTextNode(text))
+
+        document.getElementById('items').appendChild(li)
+
+        document.getElementById('newItemText').value = '';
+    } else {
+        document.getElementById('error').textContent = 'no input';
+    }
+
 }
