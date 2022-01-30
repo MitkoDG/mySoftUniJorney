13dbff13ce42ac4056d871f64ba6d45a2744ada0
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-01-30 20:02:57 +0200

    DOM

--- JS Advanced/DOM/events/06. Furniture/solution.js
+++ JS Advanced/DOM/events/06. Furniture/solution.js
@@ -1,4 +1,61 @@
 function solve() {
+  let textArea = Array.from(document.querySelectorAll('textarea'));
+  Array.from(document.querySelectorAll('button')).forEach(btn => btn.addEventListener('click', generate));
+  let table = document.querySelector('tbody');
 
-  //TODO...
-}
+  function generate(event) {
+     if (textArea.value!=""){
+       if(event.target.textContent==="Generate"){
+         let inputAreaText=JSON.parse(textArea[0].value)
+         inputAreaText.forEach((el)=>{
+           let newRow=document.createElement('tr')
+           let td1=document.createElement('td')
+           let image1=document.createElement('img') 
+           image1.setAttribute('src',`${el.img}`)
+           td1.appendChild(image1)
+
+           let td2=document.createElement('td')
+           let furnitureName=document.createElement('p')
+           furnitureName.textContent=`${el.name}`
+           td2.appendChild(furnitureName)
+
+          let td3=document.createElement('td')
+          let price=document.createElement('p')
+          price.textContent=`${el.price}`
+          td3.appendChild(price)
+
+          let td4=document.createElement('td')
+          let decorationFactor=document.createElement('p')
+          decorationFactor.textContent=`${el.decFactor}`
+          td4.appendChild(decorationFactor)
+          
+         let td5=document.createElement('td')
+         let checkBox=document.createElement('input')
+         checkBox.setAttribute("type","checkbox")
+         td5.appendChild(checkBox)
+           newRow.appendChild(td1)
+           newRow.appendChild(td2)
+           newRow.appendChild(td3)
+           newRow.appendChild(td4)
+           newRow.appendChild(td5)
+           table.appendChild(newRow)
+         })
+       }else{
+          let totalPrice=0;
+          let decorationFactor=0;
+          let names=[];
+          let isChecked=Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
+          isChecked.forEach((el)=>{
+            let mainRow=el.parentNode.parentNode
+            names.push(mainRow.children[1].textContent)
+            totalPrice+=Number(mainRow.children[2].textContent)
+            decorationFactor+=Number(mainRow.children[3].textContent)
+          })
+          textArea[1].textContent+=`Bought furniture: ${names.join(", ")}\n`
+          textArea[1].textContent+=`Total price: ${totalPrice.toFixed(2)}\n`
+          textArea[1].textContent+=`Average decoration factor: ${decorationFactor/names.length}`
+       }
+     }
+
+  };
+};
---
+++ JS Advanced/exam/preparation/01. Travel Agency/solution.js
@@ -0,0 +1,90 @@
+
+window.addEventListener('load', solution);
+
+function solution() {
+
+  let [fullName, email, fNumber, adress, postalCode] = document.querySelectorAll('div input');
+
+  let labelsArray = Array.from(document.querySelectorAll('div label'));
+
+  let button = document.querySelector('input[value="Submit"]');
+  let ulElement = document.getElementById('infoPreview');
+
+  let [btnEdit, btnCont] = ulElement.parentElement.querySelectorAll('input[type="button"]');
+  let obj = {};
+
+  button.addEventListener('click', addInfo);
+
+  function addInfo(ev) {
+
+    [fullName, email, fNumber, adress, postalCode].forEach((el, index) => {
+
+      obj[el.id] = el.value;
+
+      if (fullName.value && email.value) {
+        if (el.value !== '') {
+          createEl('li', (labelsArray[index]).textContent, el.value, ulElement);
+        }
+        ev.target.disabled = true;
+        Array.from(ulElement.parentElement.querySelectorAll('input[type="button"]'))
+          .map(el => el.disabled = false);
+      }
+    });
+    [fullName, email, fNumber, adress, postalCode].map(el => el.value = "");
+  }
+  btnEdit.addEventListener('click', edit);
+
+  function edit(ev) {
+
+    let liArrPrev = Array.from(ulElement.children);
+
+    liArrPrev.forEach((li) => {
+      li.remove();
+    });
+
+    Object.keys(obj).forEach((key) => {
+      [fullName, email, fNumber, adress, postalCode].forEach((el) => {
+
+        if (key === el.id) {
+          el.value = obj[el.id];
+        }
+      });
+    });
+    btnEdit.disabled = true;
+    btnCont.disabled = true;
+    button.disabled = false;
+  }
+
+  btnCont.addEventListener('click', removeAll);
+
+  function removeAll(ev) {
+    //noob way
+    // let elemenets = document.getElementById("form");
+    // elemenets.parentNode.removeChild(elemenets);
+    // let elemenets2 = document.getElementById("information");
+    // elemenets2.parentNode.removeChild(elemenets2);
+    // let element3 = document.querySelector("footer");
+    // element3.parentNode.removeChild(element3);
+    // let element4 = document.querySelector("h1");
+    // element4.parentNode.removeChild(element4);
+    // let element5 = document.querySelector("h4");
+    // element5.parentNode.removeChild(element5);
+
+    // let elH3 = document.createElement("h3");
+    // elH3.textContent = "Thank you for your reservation!";
+    // document.getElementById("block").appendChild(elH3);
+    const blockDiv = Array.from(document.querySelectorAll('#block'));
+    let newH3 = document.createElement('h3');
+    newH3.textContent = "Thank you for your reservation!";
+
+    blockDiv.forEach((el) => {
+      el.replaceChildren(newH3);
+    })
+  }
+
+  function createEl(type, key, textContent, curParent) {
+    let el = document.createElement(type);
+    el.textContent = `${key} ${textContent}`;
+    curParent.appendChild(el);
+  }
+}
