ec8c82785ad7d7ec29c076e4c6416fa9f20a9ae1
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-02-02 23:15:20 +0200

    DOM

--- JS Advanced/DOM/part II/01. List-Of-Items/app.js
+++ JS Advanced/DOM/part II/01. List-Of-Items/app.js
@@ -1,9 +1,22 @@
 function addItem() {
-    let inputElement = document.getElementById('newItemText');
-    let itemsElement = document.getElementById('items');
+  let inputElement = document.getElementById("newItemText");
+  let itemsElement = document.getElementById("items");
 
-    let liElement = document.createElement('li');
-    liElement.textContent = inputElement.value;
+  let liElement = document.createElement("li");
+  liElement.textContent = inputElement.value;
 
-    itemsElement.appendChild(liElement);
+  itemsElement.appendChild(liElement);
 }
+
+// function addItem() {
+//   let ulElement = document.getElementById("items");
+//   let liElement = document.createElement("li");
+//   let inputText = document.getElementById("newItemText").value;
+//   if (inputText == "") {
+//     throw alert("No input");
+//   } else {
+//     liElement.textContent = inputText;
+//     ulElement.appendChild(liElement);
+//     document.getElementById("newItemText").value = "";
+//   }
+// }
--- JS Advanced/DOM/part II/02. Delete-From-Table/app.js
+++ JS Advanced/DOM/part II/02. Delete-From-Table/app.js
@@ -1,11 +1,11 @@
 function deleteByEmail() {
-    let emailInputElement = document.querySelector('input[name="email"]');
     let emailCellElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let emailInputElement = document.querySelector('input[name="email"]');
     let resultElement = document.getElementById('result');
 
     let emailElements = Array.from(emailCellElements);
     let targetElement = emailElements.find(x => x.textContent == emailInputElement.value)
-
+    console.log(targetElement);
     if (targetElement) {
         targetElement.parentElement.remove();
 
