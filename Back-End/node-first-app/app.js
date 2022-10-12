ab0a574fb5b4565891b931eeea0bfd865706a722
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-12 15:53:01 +0300

    create a request

--- Back-End/node-first-app/app.js
+++ Back-End/node-first-app/app.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
-function rqListener(req, res) {
-    console.log(req);
-}
+// function rqListener(req, res) {
+//     console.log(req.url, req.method, req.geaders);
+//     // process.exit()
+// }
 
-const server = http.createServer(rqListener);
+const server = http.createServer((req,res) =>{
+    const url = req.url;
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My first Page</title></head>')
+    res.write('<body><h1>My first Page by using Node JS</h1></body>')
+    res.write('</html>')
+});
 
 server.listen(3000);
