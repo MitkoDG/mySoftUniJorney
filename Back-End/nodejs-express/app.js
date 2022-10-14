c1986f7f06b67d345d7fe1fa455ed97b280e54f0
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-14 16:39:39 +0300

    handle middleware with express

--- Back-End/nodejs-express/app.js
+++ Back-End/nodejs-express/app.js
@@ -4,6 +4,20 @@ const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); // Allows to request to continue to the next middleware in line
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("Another middleware");
+    res.send("<h1>Hello from Express</h1>");
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+// alternative of the above
+
+app.listen(3000)
