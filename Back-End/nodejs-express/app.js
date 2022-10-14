006a688bcadf1203eeab75593e37ae8b10ced209
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-14 16:58:22 +0300

    redirect with express

--- Back-End/nodejs-express/app.js
+++ Back-End/nodejs-express/app.js
@@ -1,15 +1,27 @@
 const http = require('http');
 
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((req, res, next) => {
-    console.log("In the middleware");
+    console.log("In the middleware and its always runs");
     next(); // Allows to request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log("Product middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     console.log("Another middleware");
     res.send("<h1>Hello from Express</h1>");
 });
