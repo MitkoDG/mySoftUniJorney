871dde5504a267d33157c4c30f49b9ef9832e904
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-17 15:05:41 +0300

    Update admin.js

--- Back-End/nodejs-express/routes/admin.js
+++ Back-End/nodejs-express/routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 
 
-router.get('/add-product', (req, res, next) => {
+router.get('/admin/add-product', (req, res, next) => {
     console.log("Product middleware");
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/admin/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
