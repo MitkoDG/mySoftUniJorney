71efb0676a5cf873108e3a84627a686a82f4fd82
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-17 14:36:30 +0300

    express Router

--- Back-End/nodejs-express/app.js
+++ Back-End/nodejs-express/app.js
@@ -4,27 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    console.log("In the middleware and its always runs");
-    next(); // Allows to request to continue to the next middleware in line
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    console.log("Product middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-});
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    console.log("Another middleware");
-    res.send("<h1>Hello from Express</h1>");
-});
 
 // const server = http.createServer(app);
 
