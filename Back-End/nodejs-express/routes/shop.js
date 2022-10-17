87f03330871dac6f4abca76d983db19df60e7b99
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-17 15:05:15 +0300

    Page 404

--- Back-End/nodejs-express/routes/shop.js
+++ Back-End/nodejs-express/routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.send("<h1>Hello from Express</h1>");
 });
 
