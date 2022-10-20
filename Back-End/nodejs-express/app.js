6b16f739d68262dc4d0836fcd3bf9b11b4a8a3aa
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-20 15:38:16 +0300

    added static folder for css

--- Back-End/nodejs-express/app.js
+++ Back-End/nodejs-express/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req,res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
