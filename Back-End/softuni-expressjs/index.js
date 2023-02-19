cccdbde8ba94a19280c1749cbaa246ce217c8648
MitkoDG <retimid@abv.bg>
2023-02-19 17:55:43 +0200

    adding status to the response

--- Back-End/softuni-expressjs/index.js
+++ Back-End/softuni-expressjs/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Hello buddy!');
+    // res.status(200);
+    res.send(200, 'Hello buddy!');
 });
 
 app.listen(3000, (err) => console.log('Server listening on port 3000'));
