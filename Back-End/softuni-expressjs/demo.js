50712673d3b04bd1fb6cc200b57f5f3ae987d4ed
MitkoDG <retimid@abv.bg>
2023-02-20 21:58:28 +0200

    Update demo.js

--- Back-End/softuni-expressjs/demo.js
+++ Back-End/softuni-expressjs/demo.js
@@ -1,14 +1,26 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
-console.log('before');
-handleFiles();
-console.log('after');
+// console.log('before');
+// handleFiles();
+// console.log('after');
 
-function handleFiles() {
-    fs.readFile('./package.json', (err, data) => {
+// function handleFiles() {
+//     fs.readFile('./package.json', (err, data) => {
 
-        console.log(data.toString());
-    });
+//         console.log(data.toString());
+//     });
 
+// }
+
+demo();
+
+async function demo() {
+    const files = await fs.readdir('.');
+    console.log(files);
 }
 
+// delete files
+
+// async function unlinkFile() {
+//     await fs.unlink('./package_copy.json');
+// }
