02299f0cd76369e91c7d30b87839d571ddb3aa72
MitkoDG <retimid@abv.bg>
2023-02-24 23:08:21 +0200

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -85,3 +85,16 @@ function countTrue(arr) {
 }
 
 countTrue([true, false, false, true, false]) // ➞ 2
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// function giveMeSomething(a) {
+// 	return `something ${a}`
+// }
+
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+// giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+// giveMeSomething("something") ➞ "something something"
+
