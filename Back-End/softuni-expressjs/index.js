59c07bdd1b8d95d00c55180acb1af6a90d5b62c9
MitkoDG <retimid@abv.bg>
2023-02-26 22:11:05 +0200

    Update index.js

--- Back-End/softuni-expressjs/index.js
+++ Back-End/softuni-expressjs/index.js
@@ -4,7 +4,29 @@ const app = express();
 
 app.get('/', (req,res) => {
     // res.status(200);
-    res.send(200, 'Hello buddy!');
+    res.status(200);
+    res.send('Hello buddy!');
+});
+
+app.post('/catalog/:productId/details', (req,res) => {
+    console.log(req.params);
+    res.status(201);
+    res.send('Post created!');
+});
+
+app.get('/catalog', (req,res) => {
+    res.status(200);
+    res.send('Catalog Page!');
+});
+
+app.post('/catalog', (req,res) => {
+    res.status(201);
+    res.send('Post created!');
+});
+
+app.all('*', (req,res) => {
+    res.status(404);
+    res.send('Page not found => ' + req.method);
 });
 
 app.listen(3000, (err) => console.log('Server listening on port 3000'));
