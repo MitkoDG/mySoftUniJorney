53675732d93db8adfa4c092947e545ac25aa8dac
MitkoDG <retimid@abv.bg>
2023-03-01 22:41:07 +0200

    added first template

--- Back-End/softuni-templating/server.js
+++ Back-End/softuni-templating/server.js
@@ -5,9 +5,11 @@ const app = express();
 var hbs = exphbs.create({defaultLayout: 'main'});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.send('Its working')
+    // res.send('Its working')
+    res.render('home')
 })
 
 app.listen(3000, (err) => console.log('Server listening on port 3000'));
