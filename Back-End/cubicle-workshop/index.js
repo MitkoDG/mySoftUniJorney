bc4c18a408fb790e97a212469a2ef084b99b8f5c
MitkoDG <retimid@abv.bg>
2023-03-05 23:32:05 +0200

    initial commit

---
+++ Back-End/cubicle-workshop/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const catalog = require('./controllers/catalog');
+
+const app = express();
+const port = 3000;
+
+app.engine('hbs', hbs({
+    extends: 'hbs'
+}));
+// app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+app.use('static', express.static('static'))
+
+app.get('/', catalog);
+
+app.listen(port, console.log(`Listening on port ${port}! Now its up to you...`));
