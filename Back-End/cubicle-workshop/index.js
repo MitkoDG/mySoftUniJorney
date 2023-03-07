840762af79015b5c1b38b45013eae2c825910fd2
MitkoDG <retimid@abv.bg>
2023-03-07 22:14:29 +0200

    adding the controllers

--- Back-End/cubicle-workshop/index.js
+++ Back-End/cubicle-workshop/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const { about } = require('./controllers/about');
 const { catalog } = require('./controllers/catalog');
+const { create, post } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { notFound } = require('./controllers/notFound');
 
 const app = express();
 const port = 3000;
@@ -14,5 +18,10 @@ app.set('view engine', 'hbs');
 app.use('static', express.static('static'))
 
 app.get('/', catalog);
+app.get('/about', about);
+app.get('/details/:id', details);
+app.get('/create', create);
+app.post('/create', post);
+app.all('*', notFound);
 
 app.listen(port, console.log(`Listening on port ${port}! Now its up to you...`));
