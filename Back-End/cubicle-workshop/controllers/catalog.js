7d6f42df7fae58c5666579225ee10e73831b6796
MitkoDG <retimid@abv.bg>
2023-03-09 22:43:17 +0200

    render the cubes with getAll

--- Back-End/cubicle-workshop/controllers/catalog.js
+++ Back-End/cubicle-workshop/controllers/catalog.js
@@ -1,5 +1,10 @@
 module.exports = {
-    catalog: (req, res) => {
-        res.render('index', { title: 'Cubicle'});
+    catalog: async (req, res) => {
+        const cubes = req.storage.getAll();
+        const ctx = {
+            title: 'Cubicle',
+            cubes
+        }
+        res.render('index', ctx);
     }
 };
