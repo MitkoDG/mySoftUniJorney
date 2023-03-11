786217ab5181fe6a6590b1a5ec218a384dcac271
MitkoDG <retimid@abv.bg>
2023-03-11 22:48:19 +0200

    added values so can save the search values

--- Back-End/cubicle-workshop/controllers/catalog.js
+++ Back-End/cubicle-workshop/controllers/catalog.js
@@ -1,13 +1,13 @@
 module.exports = {
     catalog: async (req, res) => {
-        const cubes = req.storage.getAll(req.query);
+        const cubes = await req.storage.getAll(req.query);
 
         const ctx = {
             title: 'Cubicle',
             cubes,
-            search: req.query.search,
-            from: req.query.from,
-            to: req.query.to
+            search: req.query.search || '',
+            from: req.query.from || '',
+            to: req.query.to || ''
         }
         res.render('index', ctx);
     }
