a3275d8b5ff1bc605b416194e5dbf37b7f74771a
MitkoDG <retimid@abv.bg>
2023-03-15 21:40:32 +0200

    adding See more button and JS to the Cube template

--- Back-End/cubicle-workshop/js/app.js
+++ Back-End/cubicle-workshop/js/app.js
@@ -1,1 +1,13 @@
-alert('it works');
+document.querySelector('.cube-list').addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('more')) {
+        const desc = target.parentNode.querySelector('.cube-description')
+        if (desc.style.display == 'block') {
+            desc.style.display == 'none';
+            target.textContent = 'See more'
+        } else {
+            desc.style.display == 'block';
+            target.textContent = 'Hide'
+        }
+    }
+});
