115da60a43b9f4f134a16b795aa352f187311940
MitkoDG <retimid@abv.bg>
2023-03-19 22:03:23 +0200

    find by value

--- Back-End/mongodb/index.js
+++ Back-End/mongodb/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Cat = require('./models/Cat');
+const Person = require('./models/Person');
 
 start();
 
@@ -7,25 +8,25 @@ async function start() {
     // const uri = 'mongodb+srv://retimid:password@ddgtest.w6g8wni.mongodb.net/test'
     const uri = 'mongodb://127.0.0.1:27017/testdb';
 
-    mongoose.connect(uri, {
+    const client = mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
     console.log('Connected to the database - Successful !');
 
+    // const data = await Person.find({ firstName: 'Peter' });
+    const data = await Person.find({ _id: '6415e6372c310cc411d1dddb' });
+    console.log(data);
 
-    try {
-        const comeCat = new Cat({
-            name: 'Fluffy',
-            color: 'White'
-        });
-        await someCat.save();
-    } catch (error) {
-        console.log(error.message);
-    }
-
- 
-
+    // try {
+    //     const comeCat = new Cat({
+    //         name: 'Fluffy',
+    //         color: 'White'
+    //     });
+    //     await someCat.save();
+    // } catch (error) {
+    //     console.log(error.message);
+    // }
 
 }
 
--- Back-End/mongodb/models/Person.js
+++ Back-End/mongodb/models/Person.js
@@ -7,21 +7,21 @@ const personSchema = new mongoose.Schema({
 });
 
 
-const person1 = new Person({
-    firstName: 'Peter',
-    lastName: 'Jacky',
-    age: 29
-});
+// const person1 = new Person({
+//     firstName: 'Peter',
+//     lastName: 'Jacky',
+//     age: 29
+// });
 
+// await person1.save();
 const Person = mongoose.model('Person', personSchema);
 
-await person1.save();
-const people = await Person.find({});
+// const people = await Person.find({});
 
 personSchema.methods.sayHi = function () {
     console.log(`My name is ${this.firstName} and I am ${this.age} years old.`);
 };
 
-console.log(people);
+// console.log(people);
 
 module.exports = Person;
