dea5bf04bb9467936eaf55b8bd25c8a7ee05c482
MitkoDG <retimid@abv.bg>
2023-03-28 22:58:31 +0300

    Implemented stickers

--- Back-End/cubicle-part2/models/Cube.js
+++ Back-End/cubicle-part2/models/Cube.js
@@ -4,9 +4,10 @@ const { Schema, model } = require('mongoose');
 const schema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true, maxLength: 500 },
-    imageUrl: { type: String, required: true, match: /^(https?:\/\/)/gm },
+    imageUrl: { type: String, required: true, match: /^https?:\/\// },
     difficulty: { type: Number, min: 1, max: 6 },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    accessories: [{ type: Schema.Types.ObjectId, ref: 'Accessory' }]
 });
 
 module.exports = model('Cube', schema);
