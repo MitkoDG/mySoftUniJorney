7d0819e6f29cce69d06e45a2427479aa9e259ae1
MitkoDG <retimid@abv.bg>
2023-03-29 21:48:20 +0300

    Housekeeping; added auth, msg by VSC

--- Back-End/cubicle-part2/config/database.js
+++ Back-End/cubicle-part2/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports = (app) => {
     return new Promise((resolve, reject) => {
-        mongoose.connect('mongodb://127.0.0.1:27017/cubicle', {
+        mongoose.connect('mongodb://localhost:27017/cubicle', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
