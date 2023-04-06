ee32d8f381ff28ad844335d42ed6ff6a634e46af
MitkoDG <retimid@abv.bg>
2023-04-06 22:53:22 +0300

    create session middleware; added visit counter

--- Back-End/authentication/server.js
+++ Back-End/authentication/server.js
@@ -4,20 +4,42 @@ const app = express();
 
 const sessions = {};
 
-app.get('/', (req, res) => {
-    let session = {};
-    if (req.headers.cookie) {
-        const id = req.headers.cookie.split('=')[1];
-        session = sessions[id];
-        console.log('Existing session', session);
-    } else {
-      const id = ('00000000' + (Math.random() * 99999999 | 0).toString(16)).slice(-8);
-      sessions[id] = session;
-      res.setHeader('Set-Cookie', `sessionId=${id}`);
-      console.log('New user, generated session with ID', id);
-    };
+function mySessionStorage(req, res, next) {
+  let session = {};
+  if (req.headers.cookie) {
+    const id = req.headers.cookie.split('=')[1];
+    if (sessions[id] == undefined) {
+      console.log('Invalid session cookie', session);
 
-    res.send(`<h1>Hello</h1><p>Your session has data ${JSON.stringify(session)}</p>`)
+    } else {
+      session = sessions[id];
+      console.log('Existing session', session);
+    }
+
+  } else {
+    createSession();
+  };
+
+  req.session = session;
+
+  next();
+
+  function createSession() {
+    const id = ('00000000' + (Math.random() * 99999999 | 0).toString(16)).slice(-8);
+    sessions[id] = session;
+    res.setHeader('Set-Cookie', `sessionId=${id}`);
+    console.log('New user, generated session with ID', id);
+
+    session.visited = 0;
+  }
+}
+
+app.use(mySessionStorage);
+
+app.get('/', (req, res) => {
+
+  session.visited++;
+  res.send(`<h1>Hello</h1><p>Your session has data ${JSON.stringify(session)}</p>`)
 });
 
-app.listen(3000, ()=>{console.log('Server listening on port 3000');});
+app.listen(3000, () => { console.log('Server listening on port 3000'); });
