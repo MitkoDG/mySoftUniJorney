1c9e68d8651c734998ead6d983dcd546fc424b27
MitkoDG <retimid@abv.bg>
2023-04-27 22:39:16 +0300

    config register and login routes

---
+++ Back-End/booking-exam/controllers/authController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res)=>{
+    res.render('register');
+});
+
+router.get('/login', (req, res)=>{
+    res.render('login');
+});
+
+module.exports = router;
--- Back-End/booking-exam/index.js
+++ Back-End/booking-exam/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { PORT } = require('./config/index');
 const databaseConfig = require('./config/database');
 const expressConfig = require('./config/express');
+const routesConfig = require('./config/routes');
 // const userService = require('./services/user');
 // const authMiddleware = require('./middlewares/auth');
 
@@ -11,9 +12,10 @@ start();
 
 async function start() {
     const app = express();
-    
+
     await databaseConfig(app);
     expressConfig(app);
+    routesConfig(app);
 
     app.get('/', (req, res) => res.send('It works'));
 
